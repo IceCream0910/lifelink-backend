67f0f77f53bcbf476766b4fa66438ac49f4582c9
icecream0910 <taein2370@naver.com>
2024-11-17 02:14:10 +0900

    update: cors policy

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://lifelink.yuntae.in'],
-    credentials: false
+    allowedHeaders: ['content-type'],
+    origin: ['https://lifelink.yuntae.in', 'http://localhost:3000'],
+    credentials: false,
   });
   await app.listen(process.env.PORT ?? 3000);
 }
