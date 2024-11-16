e53f052cf427275a41f126bc1613284ecb3350cc
icecream0910 <taein2370@naver.com>
2024-11-17 02:24:12 +0900

    fix: cors policy is not worked

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    allowedHeaders: ['content-type'],
-    origin: ['https://lifelink.yuntae.in', 'http://localhost:3000'],
-    credentials: false,
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
   });
   await app.listen(process.env.PORT ?? 3000);
 }
